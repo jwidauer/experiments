c56cb20279081a2f345588233cc3e8c8065e2d90
Jakob Widauer <jakob.widauer@gmail.com>
2025-06-08 15:47:14 +0200

    refactor: use deducing this in uninitialized_array

--- channels/cpp/src/main.cpp
+++ channels/cpp/src/main.cpp
@@ -1,11 +1,21 @@
-#include <string>
-
-#include "mpsc.hpp"
 #include "spsc.hpp"
+#include "uninitialized_array.hpp"
+
+struct Foo {
+  // Make Foo nontrivial to test the uninitialized array
+  Foo() : x(0), y(0.0), z('a') {}
+
+  int x;
+  double y;
+  char z;
+};
 
 auto main() -> int {
   moodycamel::SPSC<int, 10> queue;
 
-  std::string s{"test"};
+  UninitializedArray<Foo, 10> arr;
+  auto f = arr[0];
+  [[maybe_unused]] auto end = arr.end();
+
   return 0;
 }
--- channels/cpp/src/normal_iterator.hpp
+++ channels/cpp/src/normal_iterator.hpp
@@ -17,9 +17,6 @@ class NormalIterator {
 
   using traits_type = std::iterator_traits<Iterator>;
 
-  template <typename Iter>
-  using convertible_from = std::enable_if_t<std::is_convertible_v<Iter, Iterator>>;
-
  public:
   using iterator_type = Iterator;
   using iterator_category = typename traits_type::iterator_category;
@@ -28,15 +25,14 @@ class NormalIterator {
   using reference = typename traits_type::reference;
   using pointer = typename traits_type::pointer;
 
-  using iterator_concept = std::__detail::__iter_concept<Iterator>;
+  constexpr NormalIterator() noexcept : current{Iterator{}} {}
 
-  constexpr NormalIterator() noexcept : current(Iterator()) {}
-
-  explicit constexpr NormalIterator(const Iterator& i) noexcept : current(i) {}
+  explicit constexpr NormalIterator(const Iterator& i) noexcept : current{i} {}
+  explicit constexpr NormalIterator(Iterator&& i) noexcept : current{std::move(i)} {}
 
   // Allow iterator to const_iterator conversion
-  template <typename Iter, typename = convertible_from<Iter>>
-  constexpr explicit NormalIterator(const NormalIterator<Iter, Container>& i) noexcept : current(i.base()) {}
+  template <std::convertible_to<Iterator> Iter>
+  constexpr explicit NormalIterator(const NormalIterator<Iter, Container>& i) noexcept : current{i.base()} {}
 
   // Forward iterator requirements
   constexpr auto operator*() const noexcept -> reference { return *current; }
@@ -78,33 +74,6 @@ class NormalIterator {
   constexpr auto base() const noexcept -> const Iterator& { return current; }
 };
 
-template <typename T>
-concept convertiable_to_bool = std::convertible_to<T, bool>;
-
-template <typename T>
-concept boolean_testable = convertiable_to_bool<T> && requires(T&& t) {
-  { !static_cast<T &&>(t) } -> convertiable_to_bool;
-};
-
-template <typename T, typename U>
-[[nodiscard]] constexpr auto cmp(const T& t, const U& u) noexcept(_S_noexcept<T, U>())
-  requires requires {
-    { t < u } -> boolean_testable;
-    { u < t } -> boolean_testable;
-  }
-{
-  if constexpr (std::three_way_comparable_with<T, U>)
-    return t <=> u;
-  else {
-    if (t < u) return std::weak_ordering::less;
-    if (u < t) return std::weak_ordering::greater;
-
-    return std::weak_ordering::equivalent;
-  }
-}
-template <typename T, typename U = T>
-using cmp_result_t = decltype(cmp(std::declval<T&>(), std::declval<U&>()));
-
 template <typename IterL, typename IterR, typename Container>
 [[nodiscard]] constexpr auto operator==(const NormalIterator<IterL, Container>& lhs,
                                         const NormalIterator<IterR, Container>& rhs) noexcept(noexcept(lhs.base() ==
@@ -118,11 +87,11 @@ template <typename IterL, typename IterR, typename Container>
 }
 
 template <typename IterL, typename IterR, typename Container>
-[[nodiscard]] constexpr auto operator<=>(
-    const NormalIterator<IterL, Container>& lhs,
-    const NormalIterator<IterR, Container>& rhs) noexcept(noexcept(cmp(lhs.base(), rhs.base())))
-    -> cmp_result_t<IterR, IterL> {
-  return cmp(lhs.base(), rhs.base());
+[[nodiscard]] constexpr auto operator<=>(const NormalIterator<IterL, Container>& lhs,
+                                         const NormalIterator<IterR, Container>& rhs) noexcept(noexcept(lhs.base() <=>
+                                                                                                        rhs.base()))
+    -> decltype(lhs.base() <=> rhs.base()) {
+  return lhs.base() <=> rhs.base();
 }
 
 template <typename Iter, typename Container>
@@ -139,10 +108,10 @@ template <typename Iter, typename Container>
 
 template <typename Iter, typename Container>
 [[nodiscard]] constexpr auto operator<=>(const NormalIterator<Iter, Container>& lhs,
-                                         const NormalIterator<Iter, Container>& rhs) noexcept(noexcept(cmp(lhs.base(),
-                                                                                                           rhs.base())))
-    -> cmp_result_t<Iter> {
-  return cmp(lhs.base(), rhs.base());
+                                         const NormalIterator<Iter, Container>& rhs) noexcept(noexcept(lhs.base() <=>
+                                                                                                       rhs.base()))
+    -> decltype(lhs.base() <=> rhs.base()) {
+  return lhs.base() <=> rhs.base();
 }
 
 template <typename IterL, typename IterR, typename Container>
@@ -164,6 +133,11 @@ template <typename Iter, typename Container>
                                        const NormalIterator<Iter, Container>& i) noexcept
     -> NormalIterator<Iter, Container> {
   return NormalIterator<Iter, Container>(i.base() + n);
+}
+
+template <typename Container, typename Iter>
+[[nodiscard]] constexpr auto make_normal_iterator(Iter&& i) noexcept -> NormalIterator<Iter, Container> {
+  return NormalIterator<Iter, Container>(std::forward<Iter>(i));
 }
 
 #endif  // INCLUDE_SRC_NORMAL_ITERATOR_HPP_
