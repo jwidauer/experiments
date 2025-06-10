034231123340ca844cec9b91ec278a210467cff2
Jakob Widauer <jakob.widauer@gmail.com>
2025-06-10 18:10:16 +0200

    refactor: make spsc fixed size

--- channels/cpp/src/main.cpp
+++ channels/cpp/src/main.cpp
@@ -1,21 +1,13 @@
 #include "spsc.hpp"
-#include "uninitialized_array.hpp"
 
 struct Foo {
-  // Make Foo nontrivial to test the uninitialized array
-  Foo() : x(0), y(0.0), z('a') {}
-
-  int x;
-  double y;
+  double x;
+  int y;
   char z;
 };
 
 auto main() -> int {
-  moodycamel::SPSC<int, 10> queue;
-
-  UninitializedArray<Foo, 10> arr;
-  auto f = arr[0];
-  [[maybe_unused]] auto end = arr.end();
+  moodycamel::SPSC<Foo, 8> queue;
 
   return 0;
 }
--- channels/cpp/src/spsc.hpp
+++ channels/cpp/src/spsc.hpp
@@ -8,28 +8,25 @@
 #pragma once
 
 #include <atomic>
+#include <cmath>
 #include <memory>
 #include <new>
 #include <utility>
 
-#ifndef MOODYCAMEL_CACHE_LINE_SIZE
-#define MOODYCAMEL_CACHE_LINE_SIZE std::hardware_destructive_interference_size
-#endif
+#include "uninitialized_array.hpp"
 
 namespace moodycamel {
 
 template <typename T, std::size_t N>
 class SPSC {
  public:
-  using value_type = T;
+  using value_type = T;  // NOLINT(readability-identifier-naming)
 
-  explicit SPSC() : maxcap_{N}, slots_{static_cast<ssize_t>(N)}, items_{0} {
-    // Round capacity up to power of two to compute modulo mask.
-    auto capacity = std::bit_ceil(N);
-    mask_ = capacity++;
-    rawData_ = static_cast<char*>(std::malloc((capacity * sizeof(T)) + std::alignment_of_v<T> - 1));
-    data_ = align_for<T>(rawData_);
-  }
+  static constexpr std::size_t capacity =
+      std::bit_ceil(N);                              // round up to next power of two for efficient modulo operations
+  static constexpr std::size_t mask = capacity + 1;  // mask for circular buffer (power of two minus one)
+
+  SPSC() = default;
 
   SPSC(SPSC&& other) noexcept : slots_{0}, items_{0} { swap(other); }
 
@@ -38,8 +35,7 @@ class SPSC {
   // Note: The queue should not be accessed concurrently while it's
   // being deleted. It's up to the user to synchronize this.
   ~SPSC() {
-    for (std::size_t i = 0, n = size(); i != n; ++i) std::destroy_at(at((next_item_ + i) & mask_));
-    std::free(rawData_);
+    for (std::size_t i = 0, n = size(); i != n; ++i) std::destroy_at(at(next_item_ + i));
   }
 
   auto operator=(SPSC&& other) noexcept -> SPSC& {
@@ -49,26 +45,13 @@ class SPSC {
 
   auto operator=(SPSC const&) -> SPSC& = delete;
 
-  // Swaps the contents of this buffer with the contents of another.
-  // Not thread-safe.
-  void swap(SPSC& other) noexcept {
-    std::swap(maxcap_, other.maxcap_);
-    std::swap(mask_, other.mask_);
-    std::swap(rawData_, other.rawData_);
-    std::swap(data_, other.data_);
-    slots_ = other.slots_.exchange(slots_);
-    items_ = other.items_.exchange(items_);
-    std::swap(next_slot_, other.next_slot_);
-    std::swap(next_item_, other.next_item_);
-  }
-
   // Enqueues a single item (by copying it).
   // Fails if not enough room to enqueue.
   // Thread-safe when called by producer thread.
   // No exception guarantee (state will be corrupted) if constructor of T throws.
   auto try_enqueue(T const& item) -> bool {
     if (slots_.load(std::memory_order::relaxed) <= 0) return false;
-    slots_.fetch_add(-1, std::memory_order::acquire);
+    slots_.fetch_sub(1, std::memory_order::acquire);
 
     inner_enqueue(item);
     return true;
@@ -80,7 +63,7 @@ class SPSC {
   // No exception guarantee (state will be corrupted) if constructor of T throws.
   auto try_enqueue(T&& item) -> bool {
     if (slots_.load(std::memory_order::relaxed) <= 0) return false;
-    slots_.fetch_add(-1, std::memory_order::acquire);
+    slots_.fetch_sub(1, std::memory_order::acquire);
 
     inner_enqueue(std::move(item));
     return true;
@@ -93,7 +76,7 @@ class SPSC {
   template <typename U>
   auto try_dequeue(U& item) -> bool {
     if (items_.load(std::memory_order::relaxed) <= 0) return false;
-    items_.fetch_add(-1, std::memory_order::acquire);
+    items_.fetch_sub(1, std::memory_order::acquire);
 
     inner_dequeue(item);
     return true;
@@ -105,14 +88,14 @@ class SPSC {
   // Thread-safe when called by consumer thread.
   auto peek() -> T* {
     if (size() == 0U) return nullptr;
-    return at(next_item_ & mask_);
+    return at(next_item_);
   }
 
   // Pops the next element from the queue, if there is one.
   // Thread-safe when called by consumer thread.
   auto try_pop() -> bool {
     if (items_.load(std::memory_order::relaxed) <= 0) return false;
-    items_.fetch_add(-1, std::memory_order::acquire);
+    items_.fetch_sub(1, std::memory_order::acquire);
 
     inner_pop();
     return true;
@@ -127,23 +110,26 @@ class SPSC {
 
   // Returns the maximum number of elements that this circular buffer can hold at once.
   // Thread-safe.
-  auto max_capacity [[nodiscard]] () const -> std::size_t { return maxcap_; }
+  consteval auto max_capacity [[nodiscard]] () const -> std::size_t { return N; }
 
  private:
-  auto at [[nodiscard]] (std::size_t idx) -> T* { return reinterpret_cast<T*>(data_) + idx; }
-  auto at [[nodiscard]] (std::size_t idx) const -> const T* { return reinterpret_cast<T*>(data_) + idx; }
+  constexpr auto idx [[nodiscard]] (this auto& self, std::size_t idx) -> std::size_t { return idx & self.mask; }
+
+  constexpr auto at [[nodiscard]] (this auto& self, std::size_t idx) -> decltype(auto) {
+    return self.data_.data() + self.idx(idx);
+  }
 
   template <typename U>
   void inner_enqueue(U&& item) {
     auto i = next_slot_++;
-    std::construct_at<T>(at(i & mask_), std::forward<U>(item));
+    std::construct_at<T>(at(i), std::forward<U>(item));
     items_.fetch_add(1, std::memory_order::release);
   }
 
   template <typename U>
   void inner_dequeue(U& item) {
     auto i = next_item_++;
-    T* element = at(i & mask_);
+    T* element = at(i);
     item = std::move(*element);
     std::destroy_at(element);
     slots_.fetch_add(1, std::memory_order::release);
@@ -151,24 +137,15 @@ class SPSC {
 
   void inner_pop() {
     auto i = next_item_++;
-    std::destroy_at(at(i & mask_));
+    std::destroy_at(at(i));
     slots_.fetch_add(1, std::memory_order::release);
-  }
-
-  template <typename U>
-  static auto align_for(char* ptr) -> char* {
-    const std::size_t alignment = std::alignment_of_v<U>;
-    return ptr + ((alignment - (reinterpret_cast<std::uintptr_t>(ptr) % alignment)) % alignment);
   }
 
   static constexpr std::size_t chache_line_size = std::hardware_destructive_interference_size;
 
-  std::size_t maxcap_{};                               // actual (non-power-of-two) capacity
-  std::size_t mask_{};                                 // circular buffer capacity mask (for cheap modulo)
-  char* rawData_{};                                    // raw circular buffer memory
-  char* data_{};                                       // circular buffer memory aligned to element alignment
-  std::atomic<ssize_t> slots_;                         // number of slots currently free
-  std::atomic<ssize_t> items_;                         // number of elements currently enqueued
+  UninitializedArray<T, N> data_{};                    // circular buffer memory aligned to element alignment
+  std::atomic<ssize_t> slots_{N};                      // number of slots currently free
+  std::atomic<ssize_t> items_{0};                      // number of elements currently enqueued
   alignas(chache_line_size) std::size_t next_slot_{};  // index of next free slot to enqueue into
   alignas(chache_line_size) std::size_t next_item_{};  // index of next element to dequeue from
 };
