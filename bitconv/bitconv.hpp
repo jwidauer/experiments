e714ba5a11002f25676321a96c60ed8bbaf4111a
Jakob Widauer <jakob.widauer@gmail.com>
2026-02-01 19:12:23 +0100

    refactor: add more byte conversion methods

--- bitconv/bitconv.hpp
+++ bitconv/bitconv.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
-#include <algorithm>
 #include <array>
 #include <bit>
+#include <cassert>
+#include <cstring>
 #include <span>
+#include <type_traits>
 
 namespace bitconv {
 
@@ -32,16 +34,53 @@ constexpr auto as_bytes(const From& from) -> as_bytes_t<From> {
   return std::bit_cast<as_bytes_t<real_t>>(from);
 }
 
+template <typename From>
+  requires(should_copy_v<From>)
+constexpr auto as_mut_bytes(From from) -> as_mut_bytes_t<From> {
+  return std::bit_cast<as_mut_bytes_t<From>>(from);
+}
+
+template <typename From>
+  requires(!should_copy_v<From>)
+constexpr auto as_mut_bytes(From& from) -> as_mut_bytes_t<From> {
+  using real_t = std::remove_cvref_t<From>;
+  return std::bit_cast<as_mut_bytes_t<real_t>>(from);
+}
+
 template <typename To>
 constexpr auto from_bytes(value_or_ref_t<as_bytes_t<To>> from) -> To {
   return std::bit_cast<To>(from);
 }
 
 template <typename To>
-constexpr auto from_bytes(std::span<const std::byte> from) -> To {
-  as_mut_bytes_t<To> array{};
-  std::ranges::copy(from.first(sizeof(To)), array.begin());
-  return std::bit_cast<To>(array);
+constexpr auto from_mut_bytes(value_or_ref_t<as_mut_bytes_t<To>> from) -> To {
+  return std::bit_cast<To>(from);
+}
+
+template <typename To, std::size_t Extent>
+  requires std::is_trivially_copyable_v<To>
+constexpr auto from_bytes(std::span<const std::byte, Extent> from) -> To {
+  if constexpr (Extent != std::dynamic_extent) {
+    static_assert(from.size() == sizeof(To));
+  } else {
+    assert(from.size() == sizeof(To));
+  }
+  To out;
+  std::memcpy(&out, from.data(), sizeof(To));
+  return out;
+}
+
+template <typename To, std::size_t Extent>
+  requires std::is_trivially_copyable_v<To>
+constexpr auto from_mut_bytes(std::span<std::byte, Extent> from) -> To {
+  if constexpr (Extent != std::dynamic_extent) {
+    static_assert(from.size() == sizeof(To));
+  } else {
+    assert(from.size() == sizeof(To));
+  }
+  To out;
+  std::memcpy(&out, from.data(), sizeof(To));
+  return out;
 }
 
 }  // namespace bitconv
